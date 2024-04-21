7cc8b8b2fb76adf60b9d2e78a8402246293e7ea8
Lam Nghia <nghia.nl214022@sis.hust.edu.vn>
2024-04-21 16:32:43 +0700

    update T section & add image T Section

--- t_section.py
+++ t_section.py
@@ -7,60 +7,163 @@ from middleware import convert_f_unit, pi_l_section_convert_to_ui
 from math import pi, sqrt
 
 def CLC(rs, xs, rl, xl, Q, f):
+    # tim r virtual
     rv = min(rs, rl) * (Q * Q + 1)
     w = 2 * pi * f
 
-    #cs-l-cl t network matching
+    # chuyen zl sang rl noi tiep voi cpl
+    # chuyen zs sang rs noi tiep voi cps
+    cpl = -1 / w / xl
+    cps = -1 / w / xs
+
+    # tinh c1, l1 tu q1
     q = sqrt(rv / rs - 1) #start source matching
-    cs = 1 / w / rs / q
+    c1 = 1 / w / rs / q
     if (xs != 0):
-        if (cs == -1 / w / xs):
-            cs = float('inf')
+        if (c1 == cps):
+            c1 = float('inf')
         else:
-            cs = (cs * (-1 / w / xs)) / (cs + 1 / w / xs)
-    cs = cs * 1e12
-    # return cs
+            c1 = (c1 * cps) / (c1 - cps)
+    c1 = c1 * 1e12
+    # return c1
+    l1 = rv / w / q
 
-    ls = rv / w / q
+    # tinh c2, l2 tu q2
     q = sqrt(rv / rl - 1) #start load matching
-    cl = 1 / w / rl / q
+    c2 = 1 / w / rl / q
     if (xl != 0) :
-        if (cl == -1 / w / xs) :
-          cl = float('inf')
+        if (c2 == cpl) :
+            c2 = float('inf')
         else :
-          cl = (cl * (-1 / w / xs)) / (cl + 1 / w / xs)
-    cl = cl * 1e12
-    #return cl
+          cl = (c2 * cpl) / (c2 - cpl)
+    c2 = c2 * 1e12
+    # return c2
+    l2 = rv / w / q
 
-    ll = rv / w / q
-    l = (ll * ls) / (ll + ls)
+    l = (l2 * l1) / (l2 + l1)
     l = l * 1e9
     #return l
 
-    return [cs,l,cl,"CLC_T_Section"]
+    return [c1,l,c2,"CLC_T_Section"]
 
 def LCL(rs, xs, rl, xl, Q, f):
+    # tim r virtual
     rv = min(rs, rl) * (Q * Q + 1)
     w = 2 * pi * f
 
-    # ls-c-ll t network matching
+    # chuyen zl sang rl noi tiep voi lpl
+    # chuyen zs sang rs noi tiep voi lps
+    lpl = xl / w
+    lps = xs / w
+
+    # tinh l1, c1 tu q1
     q = sqrt(rv / rs - 1) #start source matching
-    ls = (q * rs) / w - xs / w
-    ls = ls * 1e9
-    # return ls
+    l1 = (q * rs) / w - lps
+    l1 = l1 * 1e9
+    # return l1
+    c1 = q / w / rv
 
-    cs = q / w / rv
+    # tinh l2, c2 tu q2
     q = sqrt(rv / rl - 1) #start load matching
-    ll = (q * rl) / w - xl / w
-    ll = ll * 1e9
-    # return ll
+    l2 = (q * rl) / w - lpl
+    l2 = l2 * 1e9
+    # return l2
+    c2 = q / w / rv
 
-    cl = q / w / rv
-    c = cs + cl
+    c = c1 + c2
     c = c * 1e12
     # return c
 
-    return [ls,c,ll,"LCL_T_Section"]
+    return [l1,c,l2,"LCL_T_Section"]
+
+def LCLC(rs, xs, rl, xl, Q, f):
+    # tim r virtual
+    rv = min(rs, rl) / (Q * Q + 1)
+    w = 2 * pi * f
+
+    # chuyen zl sang rl noi tiep voi cpl
+    # chuyen zs sang rs noi tiep voi lps
+    ql = xl / rl
+    qs = xs / rs
+    cpl = -1 / w / xl
+    lps = xs / w
+
+    # tinh c1, l1 tu q1
+    q = sqrt(rv / rs - 1) #start source matching
+    l1 = (q * rs) / w - lps
+    l1 = l1 * 1e9
+    # return l1
+    zc1 = rv / q
+    # c1 = q / w / rv
+
+    # tinh c2, l2 tu q2
+    q = sqrt(rv / rl - 1) #start load matching
+    c2 = 1 / w / rl / q
+    if (xl != 0) :
+        if (c2 == cpl) :
+            c2 = float('inf')
+        else :
+          cl = (c2 * cpl) / (c2 - cpl)
+    c2 = c2 * 1e12
+    # return c2
+    zl2 = rv / q
+    # l2 = rv / w / q
+
+    # so sanh zl2 va zc1 de chon l hay c
+    z_temp = (zl2 * (-zc1)) / (zl2 - zc1)
+    if (z_temp > 0):
+        l = z_temp / w
+        l = l * 1e9
+        return [l1,l,c2,"LLC_T_Section"]
+    elif (z_temp < 0):
+        c = 1 / w / z_temp
+        c = c * 1e12
+        return [l1,c,c2,"LCC_T_Section"]
+
+def CLCL(rs, xs, rl, xl, Q, f):
+    # tim r virtual
+    rv = max(rs, rl) / (Q * Q + 1)
+    w = 2 * pi * f
+
+    # chuyen zl sang rl noi tiep voi lpl
+    # chuyen zs sang rs noi tiep voi cps
+    ql = xl / rl
+    qs = xs / rs
+    lpl = xl / w
+    cps = -1 / w / xs
+
+    # tinh c1, l1 tu q1
+    q = sqrt(rv / rs - 1) #start source matching
+    c1 = 1 / w / rs / q
+    if (xs != 0):
+        if (c1 == cps):
+            c1 = float('inf')
+        else:
+            c1 = (c1 * cps) / (c1 - cps)
+    c1 = c1 * 1e12
+    # return c1
+    zl1 = rv / q
+
+    # tinh l2, c2 tu q2
+    q = sqrt(rv / rl - 1) #start load matching
+    l2 = (q * rl) / w - lpl
+    l2 = l2 * 1e9
+    # return l2
+    zc2 = rv / q
+
+    
+    # so sanh zl1 va zc2 de chon l hay c
+    z_temp = (zl1 * (-zc2)) / (zl1 - zc2)
+    if (z_temp > 0):
+        l = z_temp / w
+        l = l * 1e9
+        return [c1,l,l2,"CLL_T_Section"]
+    elif (z_temp < 0):
+        c = 1 / w / z_temp
+        c = c * 1e12
+        return [c1,c,l2,"CCL_T_Section"]
+
+
 
 
 def dc_block_handler(data):
@@ -80,6 +183,8 @@ def dc_block_handler(data):
     else:
         result = []
         result.append(CLC(rs, xs, rl, xl, Q, f))
+        result.append(LCLC(rs, xs, rl, xl, Q, f))
+        result.append(CLCL(rs, xs, rl, xl, Q, f))
         return pi_l_section_convert_to_ui(result)
 
 def dc_feed_handler(data):
